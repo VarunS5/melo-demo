e9bb94f5e977355bb502240c473e88260804c995
harshalchaudhari <harshal.chaudhari@thoughtworks.com>
2023-08-29 11:27:09 +0530

    Added duplicated lines of code

--- lib/test1.dart
+++ lib/test1.dart
@@ -1,6 +1,65 @@
 _bar() => _Foo();
 
-
 class _Foo {
   _foo() => 42;
+}
+
+String intToStringValue(int value) {
+  if (value == 1) {
+    return 'one';
+  }
+
+  if (value == 2) {
+    return 'one';
+  }
+  if (value == 3) {
+    return 'one';
+  }
+  if (value == 4) {
+    return 'one';
+  }
+  if (value == 5) {
+    return 'one';
+  }
+  if (value == 6) {
+    return 'one';
+  }
+  if (value == 7) {
+    return 'one';
+  }
+  if (value == 8) {
+    return 'one';
+  }
+  if (value == 9) {
+    return 'one';
+  }
+  if (value == 10) {
+    return 'one';
+  }
+  if (value == 11) {
+    return 'one';
+  }
+  if (value == 12) {
+    return 'one';
+  }
+  if (value == 13) {
+    return 'one';
+  }
+  if (value == 14) {
+    return 'one';
+  }
+  if (value == 15) {
+    return 'one';
+  }
+  if (value == 16) {
+    return 'one';
+  }
+  if (value == 17) {
+    return 'one';
+  }
+  if (value == 18) {
+    return 'one';
+  }
+
+  return 'Invalid';
 }
