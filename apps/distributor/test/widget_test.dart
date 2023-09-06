45580dc7500796841084ca7a633ca897f39c44d4
Rohit Karadkar <rohit.karadkar@thoughtworks.com>
2023-09-06 10:47:25 +0530

    Fix tests and add melos script for test

--- apps/distributor/test/widget_test.dart
+++ apps/distributor/test/widget_test.dart
@@ -5,21 +5,13 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:distributor/distributor_home.dart';
-import 'package:flutter/material.dart';
+import 'package:distributor/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:distributor/main.dart';
-
 void main() {
-  testWidgets('Distributor home has two buttons', (tester)  async {
-    const newOrderKey = Key("New Orders");
-    const syncOfflineOrderKey = Key("Sync offline order");
-    await tester.pumpWidget(const MaterialApp(
-      home: DistributorHome(),
-    ));
+  testWidgets('sample widget test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
 
-    expect(find.byKey(newOrderKey), findsOneWidget);
-    expect(find.byKey(syncOfflineOrderKey), findsOneWidget);
+    expect(find.text('Distributor login'), findsOneWidget);
   });
 }
--- apps/retailer/test/widget_test.dart
+++ apps/retailer/test/widget_test.dart
@@ -5,20 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:retailer/main.dart';
-import 'package:retailer/retailer_home.dart';
 
 void main() {
-  testWidgets('Retailer home has two buttons', (tester) async {
-    const newOrderKey = Key("New Orders");
-    const syncOfflineOrderKey = Key("Invoices");
-    await tester.pumpWidget(const MaterialApp(
-      home: RetailerHome(),
-    ));
-    expect(find.byKey(newOrderKey), findsOneWidget);
-    expect(find.byKey(syncOfflineOrderKey), findsOneWidget);
+  testWidgets('Sample widget test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    expect(find.text('Retailer login'), findsOneWidget);
   });
 }
